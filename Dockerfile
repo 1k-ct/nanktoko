30d8d2ba9fbfeea6b09d312d8d001fa089dcad25
1k-ct <268iop@gmail.com>
2020-08-13 17:33:42 +0900

    　

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# ベースとなるDockerイメージ指定
-FROM golang:latest
-# コンテナ内に作業ディレクトリを作成
-RUN mkdir /go/src/youtubeWeb774inc
-# コンテナログイン時のディレクトリ指定
-WORKDIR /go/src/youtubeWeb774inc
-# ホストのファイルをコンテナの作業ディレクトリに移行
-ADD . /go/src/youtubeWeb774inc
--- main.go
+++ main.go
@@ -1,43 +1,15 @@
 package main
 
 import (
-	"strconv"
-	"youtubeWeb774inc/db"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-	dbdef := db.ConnectGorm()
-	defer dbdef.Close()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
 
-	router := gin.Default()
-	//router.Static("/assets", "./assets")
-	router.LoadHTMLGlob("templates/*.html")
-
-	router.GET("/", func(ctx *gin.Context) {
-		tokos := db.GetDBContents()
-		ctx.HTML(200, "index.html", gin.H{
-			"tokos": tokos,
-		})
-	})
-	router.POST("/creat", func(ctx *gin.Context) {
-		text := ctx.PostForm("toko_text")
-		name := ctx.PostForm("toko_user_name")
-		//createdAt = ctx.PostForm("createdAt")
-		db.AddNewInDB(text, name) //, createdAt
-		ctx.Redirect(302, "/")
-	})
-	router.POST("/delete", func(ctx *gin.Context) {
-		n := ctx.PostForm("db_delete_num")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		db.DeleteDB(id)
-		ctx.Redirect(302, "/")
-	})
-	router.Run()
-
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, from Docker container!")
 }
